cb1e2a16d336838393af0a81d661d3083cde9dd1
Germán Waisvol <german_waisvol@hotmail.com>
2022-12-25 18:51:05 -0300

    primera entrega

---
+++ prueba.py
@@ -0,0 +1,8 @@
+#Mi primer programa en Python.
+
+nota_1=int(input("Ingrese la nota 1"))
+nota_2=int(input("Ingrese la nota 2"))
+nota_3=int(input("Ingrese la nota 3"))
+
+nota_final=((0.2*nota_1) + (0.3*nota_2) + (0.55*nota_3) )
+print("La nota final es:", nota_final)
