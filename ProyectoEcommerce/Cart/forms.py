890af9e071586cb0cb5dce48660f755f222b25af
Germán Waisvol <german_waisvol@hotmail.com>
2023-04-25 21:49:46 -0300

    cambios

--- ProyectoEcommerce/Cart/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class CartForm(forms.Form):
-    username = forms.CharField(max_length=100, label="Usuario")
-    products = forms.CharField(max_length=100, label="Productos")
--- ProyectoEcommerce/ProyectoEcommerce/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def welcome_page(request):
-    return render(request, 'welcome.html', context={})
-
-def our_company(request):
-    return render(request, 'us.html', context={})
-
